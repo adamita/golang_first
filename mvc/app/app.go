0cfe9eed5117a6d497fc8f344af6fa900635dacd
Adam Matis <adamita22@gmail.com>
2023-01-19 11:29:48 +0100

    ..

--- mvc/app/app.go
+++ mvc/app/app.go
@@ -1,10 +1,18 @@
 package app
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
 	"github.com/adamita/golang_first/mvc/controllers"
 )
 
-func StartApp(){
+func StartApp() {
 	http.HandleFunc("/users", controllers.GetUser)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
