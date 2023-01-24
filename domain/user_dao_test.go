4277eb37d27ab04f80a8f0d1bd8cb8b6698089fd
Adam Matis <adamita22@gmail.com>
2023-01-24 09:38:53 +0100

    Change to go mod

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -1,15 +1,22 @@
 package domain
 
-import "testing"
+import (
+	"github.com/stretchr/testify/asserts"
+	"testing"
+)
 
 func TestUserNotUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
 	if user != nil {
-		t.Error("user exists with id 0")
+		t.Error("expecting user not exists with id 0")
 	}
 
 	if err == nil {
-		t.Error("error when user id 0")
+		t.Error("expecting error when user id 0")
+	}
+
+	if err.StatusCode != http.StatusNotFound {
+		t.Error("expecting 404 not found")
 	}
 }
