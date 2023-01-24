b4248590fd29035a11247a9c0cc232d8ad3aa00c
Adam Matis <adamita22@gmail.com>
2023-01-24 10:33:49 +0100

    Change test to asserts

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -1,22 +1,18 @@
 package domain
 
 import (
-	"github.com/stretchr/testify/asserts"
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUserNotUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
-	if user != nil {
-		t.Error("expecting user not exists with id 0")
-	}
-
-	if err == nil {
-		t.Error("expecting error when user id 0")
-	}
-
-	if err.StatusCode != http.StatusNotFound {
-		t.Error("expecting 404 not found")
-	}
+	assert.Nil(t, user, "expecting user not exists with id 0")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 was not found", err.Message)
 }
