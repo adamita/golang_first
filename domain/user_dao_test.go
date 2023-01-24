7a7a832cf73f7ff81d6137de8639a1e44a3f0dc7
Adam Matis <adamita22@gmail.com>
2023-01-24 10:50:37 +0100

    Add test case

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -16,3 +16,11 @@ func TestUserNotUserFound(t *testing.T) {
 	assert.EqualValues(t, "not_found", err.Code)
 	assert.EqualValues(t, "user 0 was not found", err.Message)
 }
+
+func TestGetUserNoError(t *testing.T){
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.Id)
+}
